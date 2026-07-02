50bb6e0e94b91b686c88818278bd7310a35fcb52
Varin Thakur <varin.thakur@sap.com>
2026-07-02 08:26:29 +0000

    add HanaSparqlQAAgent

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./vectorstores/index.js";
 export * from "./graphs/index.js";
 export * from "./chains/index.js";
+export * from "./agents/index.js";
 export * from "./embeddings/index.js";
 export * from "./structured_query/index.js";
